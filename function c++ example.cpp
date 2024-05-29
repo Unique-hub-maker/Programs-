703dfb8bd127bfe0c72dc6c4b83de4cbddf05897
Unique-hub-maker <116149443+Unique-hub-maker@users.noreply.github.com>
2024-05-29 13:02:14 +0545

    Add files via upload

---
+++ function c++ example.cpp
@@ -0,0 +1,19 @@
+//Function example
+
+#include<iostream>
+using namespace std;
+void add(int a,int b)
+{
+	cout<<"Addition="<<a+b;
+	
+}
+void area(float r)
+{
+	cout<<"area="<<3.14*r*r;
+}
+int main()
+{
+	add(30,40);
+	area(7.5);
+	return 0;
+}
