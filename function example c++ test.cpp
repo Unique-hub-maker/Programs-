05d8489d95b5556a8ce011f1757049eec942bcb0
Unique-hub-maker <116149443+Unique-hub-maker@users.noreply.github.com>
2024-05-29 13:16:08 +0545

    changes

--- function example c++ test.cpp
+++ function example c++ test.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void add(int a,int b)
+void add(int a, int b)
 {
-	cout<<"Sum is ="<<a+b<<endl;
+	cout << "Sum is =" << a + b << endl;
 }
-void sub(int a,int b)
+void sub(int a, int b)
 {
-		cout<<"Sub is ="<<a-b<<endl;
-} 
-void mul(int a,int b)
-{
-	cout<<"Multiply is ="<<a*b<<endl;
+	cout << "Sub is =" << a - b << endl;
 }
-void div(float a,float b)
+void mul(int a, int b)
 {
-		cout<<"Division is ="<<a/b<<endl;
+	cout << "Multiply is =" << a * b << endl;
+}
+void div(float a, float b)
+{
+	cout << "Division is =" << a / b << endl;
 }
 int main()
 {
-	int x,y;
-	cout<<"Enter first number"<<endl;
-	cin>>x;	
-	cout<<"Enter second number"<<endl;
-	cin>>y;	
-	add(x,y);
-	sub(x,y);
-	mul(x,y);
-	div(x,y);
+	int x, y;
+	cout << "Enter first 1 number" << endl;
+	cin >> x;
+	cout << "Enter second number" << endl;
+	cin >> y;
+	add(x, y);
+	sub(x, y);
+	mul(x, y);
+	div(x, y);
 	return 0;
 }
